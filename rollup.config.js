864025e7507bef1cf5df62ceca8b4b032d1a785d
steinjun0 <1142308@naver.com>
2023-07-23 16:43:24 +0900

    add external to rollup.config.js

--- rollup.config.js
+++ rollup.config.js
@@ -1,9 +1,20 @@
 const resolve = require("@rollup/plugin-node-resolve");
 const commonjs = require("@rollup/plugin-commonjs");
 const typescript = require("@rollup/plugin-typescript");
+const builtins = require('builtin-modules');
 const dts = require("rollup-plugin-dts").default
 
 const packageJson = require("./package.json");
+
+const external = pkg => {
+  const externals = [...Object.keys({ ...packageJson.dependencies, ...packageJson.peerDependencies }), ...builtins, 'tslib', 'react'];
+  const result = externals.some(externalPkg => {
+    return pkg.startsWith(externalPkg);
+  });
+  // console.log(externals)
+  // console.log(pkg,result)
+  return result
+};
 
 module.exports = [
   {
@@ -20,6 +31,7 @@ module.exports = [
         sourcemap: true,
       },
     ],
+    external,
     plugins: [
       resolve(),
       commonjs(),
@@ -29,6 +41,7 @@ module.exports = [
   {
     input: "dist/esm/types/index.d.ts",
     output: [{ file: "dist/index.d.ts", format: "esm" }],
+    external,
     plugins: [dts()],
   },
 ];
