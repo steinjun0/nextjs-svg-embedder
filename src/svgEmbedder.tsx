aba086c6b761cd67a33cd5829be4aa4f32b95a28
steinjun0 <1142308@naver.com>
2023-07-23 19:07:44 +0900

    change force to override

--- src/svgEmbedder.tsx
+++ src/svgEmbedder.tsx
@@ -2,11 +2,11 @@ import React from "react";
 
 const startPath = __dirname.slice(0, __dirname.indexOf('.next'));
 
-export default function SvgEmbedder({ src, force = false, ...props }: { src: { src: string; }, force?: boolean; } & React.SVGProps<SVGSVGElement>) {
+export default function SvgEmbedder({ src, override = false, ...props }: { src: { src: string; }, override?: boolean; } & React.SVGProps<SVGSVGElement>) {
     const imgPath = require('path').join(startPath, src.src.replace('_next', '.next'));
     const propsString = Object.entries(props).map(([key, value]) => `${key}="${value}"`).join(' ');
     let svgString = require('fs').readFileSync(imgPath).toString().replace(/<svg/, `<svg ${propsString}`);
-    if (force) {
+    if (override) {
         Object.entries(props).forEach(([key, value]) => {
             svgString = svgString.replace(new RegExp(`${key}=".+?"`, 'g'), `${key}="${value}"`);
         }
